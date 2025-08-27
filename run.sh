f222679907cf9619cdf9cf78bc8f87f30dec34a6
Nicholas Tyler <tylern@lbl.gov>
2025-08-27 13:29:35 -0700

    Update run number

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ python utils/get_scale.py
 export MASTER_ADDR=$(hostname)
 config_file=./config/operators_poisson.yaml
 config="poisson-scale-k1_5"
-run_num="01"
+run_num="02"
 
 # path/to/logs
 results_dir=$SCRATCH/clearml_tests/results
