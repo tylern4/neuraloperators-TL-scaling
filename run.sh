aa73f407dac505f988d98777f219d95340727cb0
Nicholas Tyler <tylern@lbl.gov>
2025-08-26 17:32:26 -0700

    generate data in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+ntrain=32768
+nval=4096
+ntest=4096
+ng=144
+datapath=$PWD
+
+e1=1  # poissons diffusion eigenvalue range
+e2=5
+
+adr1=0.2 # advection to diffusion ratio range
+adr2=1
+# for AD ratio we saved a set of velocity scales that correspond to AD ration in utils/*.npy. See python script for details
+
+o1=1 # helmholtz wave number range
+o2=10
+
+python utils/gen_data_poisson.py --ntrain=$ntrain --nval=$nval --ntest=$ntest \
+                    --ng=$ng --sparse --n 128 --datapath $datapath --e1 $e1 --e2 $e2
 
 export MASTER_ADDR=$(hostname)
 config_file=./config/operators_poisson.yaml
@@ -9,6 +27,5 @@ run_num="01"
 results_dir=$SCRATCH/clearml_tests/results
 mkdir -p ${results_dir}
 
-cmd="python train.py --yaml_config=$config_file --config=$config --run_num=$run_num --root_dir=$results_dir"
 source export_DDP_vars.sh
-exec $cmd
+python train.py --yaml_config=$config_file --config=$config --run_num=$run_num --root_dir=$results_dir
